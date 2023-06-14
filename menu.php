dd5a2b3f3fe377898a4206383e57cb15438e4847
RIHU <rihu0311@gmail.com>
2023-06-14 10:37:53 +0900

    card 디자인 재생성
    카드 디자인이 계속 이상해서 새로 만들어야할듯... 반응형으로 만드려니깐 너무 어려움 ㅠㅠ 일단 자고옴

--- menu.php
+++ menu.php
@@ -26,12 +26,12 @@
             if($result->num_rows > 0) {    // 검색된 레코드가 있으면 
                     while($row = $result->fetch_assoc()) {  // 레코드 한 개를 연관배열로 가져오기 
             ?>
-            <div class="card card-1">
+            <div class="card">
                     <a href="addcart.php?pizza=<?=$row['name']?>
                             &singlemenu=<?=$row['singlemenu']?>&setmenu=<?=$row['setmenu']?>">
                             <img src="img/<?= $row['photo'] ?>"></a>
                     <div class="">
-                        <h3><?= $row['name'] ?></h3>
+                        <h3 class = "foodname"><?= $row['name'] ?></h3>
                         <h3>Single&nbsp;&nbsp;&nbsp;<?= $row['singlemenu'] ?></h3>
                         <h3>Set&nbsp;&nbsp;&nbsp;<?= $row['setmenu'] ?></h3>
                     </div>
@@ -49,12 +49,12 @@
             if($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
             ?>
-            <div class="card card-1">
+            <div class="card">
                     <a href="addcart.php?pizza=<?=$row['name']?>
                             &small=<?=$row['small']?>&large=<?=$row['large']?>">
                             <img src="img/<?= $row['photo'] ?>"></a>
                     <div class="text">
-                        <h3><?= $row['name'] ?></h3>
+                        <h3 class = "foodname"><?= $row['name'] ?></h3>
                         <h3><?= $row['small'] ?></h3>
                         <h3><?= $row['large'] ?></h3>
                     </div>
@@ -72,12 +72,12 @@
             if($result->num_rows > 0) {     
                     while($row = $result->fetch_assoc()) {
             ?>
-            <div class="card card-1">
+            <div class="card">
                     <a href="addcart.php?pizza=<?=$row['name']?>
                             &price=<?=$row['price']?>">
                             <img src="img/<?= $row['photo'] ?>"></a>
                     <div class="">
-                        <h3><?= $row['name'] ?></h3>
+                        <h3 class = "foodname"><?= $row['name'] ?></h3>
                         <h3><?= $row['price'] ?></h3>
                     </div>
             </div>
