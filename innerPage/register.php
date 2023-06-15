50770ddc4e7722ad8277e58cf333d3fa5c7b79d0
RIHU <rihu0311@gmail.com>
2023-06-15 21:12:38 +0900

    ㅁ

---
+++ innerPage/register.php
@@ -0,0 +1,28 @@
+<?php
+# 회원가입 처리 php파일
+# 1) 데이터베이스 연결
+# 2) 회원가입 데이터 읽어오기
+# 3) INSERT SQL 명령문 작성하기
+# 4) SQL 실행하고 결과 확인하기
+
+include_once('dbconn.php'); # dbconn.php파일의 내용을 읽어서 가져온다
+$email = $_POST['email'];
+$uname = $_POST['uname'];
+$pwd = $_POST['pwd'];
+$telno = $_POST['telno'];
+$regdate = date('Y/m/d');
+$sql = "insert into member values('$email','$uname','$pwd','$telno','$regdate')";
+if($conn->query($sql)) {
+    #echo "회원가입 성공";
+    #header('location: index.html');
+    #자바스크립트 구문 이동 처리
+    echo "<script>alert('Z-Burger에 성공적으로 가입했습니다.')</script>";
+    #echo "<script>location.href='index.html'</script>";
+    echo "<script>location.replace('index.php')</script>";
+}
+else {
+    #echo "회원가입 실패 : ".$conn->error;
+    echo "<script>alert('회원가입에 오류가 있습니다.')</script>";
+    echo "<script>location.replace('register.html')</script>";
+}
+?>
