36079d2b5297c1b615dc7b9aaaf76fd6ba044fe7
RIHU <rihu0311@gmail.com>
2023-06-17 06:23:07 +0900

    zorder sql

--- innerPage/ordernew.php
+++ innerPage/ordernew.php
@@ -1,5 +1,31 @@
-<?php
-session_start();
-include_once('dbconn.php');
-$email = $_SESSION['pz_uid'];
-$uname = $_SESSION['pz_uname'];
+<!DOCTYPE HTML>
+<html>
+    !DOCTYPE html>
+<html>
+<head>
+    <title>피자배달 주문</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/ordernewStyle.css">
+    </head>
+    <body>
+        <?php
+        session_start();
+        include_once('dbconn.php');
+        $uname = $_SESSION['pz_uname'];
+        $email = $_SESSION['pz_uid']
+        #새로운 주문 번호 생성. 현재 마지막 주문번호를 가져와서 순번을 하나씩 증가시킴
+        $sql = "select max(ordno) maxordno from porder";
+        $result = $conn->query($sql);
+        $row = $result-> fetch_assoc();
+        #주문번호 형태를 년원일-순번으로 정함
+        $today = date('Y').date('m').date('d'); // ex)20230617
+        
+        
+        
+        ?>
+    </body>
+</html>
+
+
+
---
+++ sql/zorder.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS `burger`;
+
+USE `burger`;
+
+DROP TABLE IF EXISTS `cart`;
+
+
+CREATE TABLE `zorder` (
+  `ordno` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `orddate` date NOT NULL,
+  `address` varchar(80) DEFAULT NULL,
+  `amount` int(11) NOT NULL,
+  `delamt` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+ PRIMARY KEY(`email`,`food`,`size`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
