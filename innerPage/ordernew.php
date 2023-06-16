84f8aeaa7cf58da6f8885a9f6cf0fbcb6e61b1be
RIHU <rihu0311@gmail.com>
2023-06-17 08:28:33 +0900

    css를 제외한 구현완료

--- innerPage/ordernew.php
+++ innerPage/ordernew.php
@@ -1,9 +1,7 @@
 <!DOCTYPE HTML>
 <html>
-    !DOCTYPE html>
-<html>
 <head>
-    <title>피자배달 주문</title>
+    <title>Z-Burger Delivery</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/ordernewStyle.css">
@@ -13,18 +11,82 @@
         session_start();
         include_once('dbconn.php');
         $uname = $_SESSION['pz_uname'];
-        $email = $_SESSION['pz_uid']
+        $email = $_SESSION['pz_uid'];
         #새로운 주문 번호 생성. 현재 마지막 주문번호를 가져와서 순번을 하나씩 증가시킴
-        $sql = "select max(ordno) maxordno from porder";
+        $sql = "select max(ordno) maxordno from zorder";
         $result = $conn->query($sql);
         $row = $result-> fetch_assoc();
         #주문번호 형태를 년원일-순번으로 정함
         $today = date('Y').date('m').date('d'); // ex)20230617
-        
-        
-        
+        #zorder 테이블에서 검색한 가장 큰 주문번호의 년월일 가져오기
+        $prefix = substr($row['maxordno'],0,strpos($row['maxordno'
+        ],'-'));
+        if($today == $prefix){ //오늘 날짜로 이미 주문한 내역이 존재
+            $no = substr($row['maxordno'], strpos($row['maxordno'],'-')+1,2);
+        $no++;
+        $ordno = $prefix . "-" . $no;
+        }
+        else //오늘 첫 주문
+            $ordno = $today."-01"; // 20230607-01
+        #장바구니에 담긴 피자들의 총 가격을 가져오기
+        $sql = "select sum(total) amount from cart where email = '$email'";
+        $result = $conn -> query($sql); //select 실행 결과는 1건 나옴
+        $row = $result -> fetch_assoc();
+        $amount = $row['amount']; //sum(price) 결과 값을 가져옴        
         ?>
-    </body>
+        
+        <h2>Z-Burger Order</h2>
+        <p>장바구니에 담긴 피자 배달 주문합니다.</p>
+        <hr>
+        <div class="container">
+          <form action="ordernewproc.php" method="post">
+            <div class="row">
+              <div class="col-25">
+                <label for="fname">주문자</label>
+              </div>
+              <div class="col-75">
+                <input type="text" name="uname" value="<?=$uname?>" readonly>
+				        <input type="text" name="email" value="<?=$email?>" hidden>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="fname">주문번호</label>
+              </div>
+              <div class= "col-75">
+                <input type="text" name="ordno" value="<?=$ordno?>" readonly>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="fname">배달주소</label>
+              </div>
+              <div class="col-75">
+                <input type="text" name="address" placeholder="Address..">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="fname">주문금액</label>
+              </div>
+              <div class="col-75">
+                <input type="text" name="amount" value="<?=$amount?>" readonly>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="fname">배달료</label>
+              </div>
+              <div class="col-75">
+                <input type="text" name="delamt" value=5000 readonly>
+              </div>
+            </div>
+		    <div class="row">
+              <input type="submit" value="배달하기">
+            </div>
+		  </form>
+		</div>
+	</body>
 </html>
 
 
---
+++ innerPage/ordernewproc.php
@@ -0,0 +1,65 @@
+<?php
+# porder 테이블에 신규주문 내역 저장한 다음 orditem 테이블에 주문하는 피자를 저장하기
+# 마스터 테이블에 먼저 레코드 추가하고 디테일 테이블에 레코드 추가하기
+include_once('dbconn.php');
+#ordernew.php에서 전달되는 데이터 가져오기
+$ordno = $_POST['ordno'];
+$email = $_POST['email'];
+$address = $_POST['address'];
+$orddate = date('Y/m/d'); //2023/05/30
+$amount = $_POST['amount'];
+$delamt = $_POST['delamt'];
+$total = $amount + $delamt;
+
+#트랜잭션 처리를 위해서 autocommit을 해제하기. autocommit은 하나의 operation 마다 자동 확정.
+$conn -> autocommit(false);
+
+$sql ="insert into zorder values ('$ordno','$email','$orddate','$address',
+    $amount, $delamt, $total)"; 
+if(!$conn->query($sql)) { # insert 실행 중에 오류가 발생했다면
+    #트랜잭션 처리 과정 중에 오류가 발생하면 롤백을 처리
+    $conn -> rollback(); #실행을 취소하기. 실행하기 전 상태로 되돌리기
+    die('주문내역 생성 중에 오류가 발생했습니다.'.$conn->error);
+}
+# 2. orditem 테이블에 장바구니 물품을 저장하기
+$sql = "select * from cart where email = '$email'";
+$result = $conn -> query($sql);
+if(!$result){ #select 오류가 발생해서 $result가 생성되지 않음
+    $con->rollback();
+    die('장바구니 검색 중에 오류 발생'. $conn->error); 
+}
+
+if($result -> num_rows > 0){
+    $seq = 0;
+    # Prepared Statement 객체를 생성
+    $zstmt = $conn->prepare("insert into orditem values(?,?,?,?,?,?)");
+    # s : string, i : int, d : double, b : blob
+    $zstmt->bind_param("sissii",$ordno,$seq,$food,$size,$qty,$price);
+
+    while($row = $result->fetch_assoc()){
+        $seq++;
+        $food = $row['food'];
+        $size = $row['size'];
+        $qty = $row['qty'];
+        $price = $row['price'];
+        #Prepared Statement 실행하기
+        $result2 = $zstmt->execute();
+        if(!$result2){
+            $conn -> rollback();
+            die('주문 상세내역 저장 중에 오류가 발생했습니다.' . $conn -> error());
+        }
+    }
+}
+# 3. cart 테이블에서 장바구니 데이터 삭제하기
+$sql = "delete from cart where email = '$email'";
+if(!$conn->query($sql)){
+    $conn -> rollback();
+    die('장바구니 데이터 삭제 중에 오류가 발생했습니다.' . $conn -> error());
+}
+
+#모든 연산이 수행되었으므로 최종 확정하기
+$conn -> commit();
+$conn -> autocommit(true); #반드시 autocommit을 설정하여야 함.
+echo "<script>alert('피자 배달 주문이 정상적으로 생성되었습니다.');";
+echo "location.href = '../index.php'</script>";
+?>
