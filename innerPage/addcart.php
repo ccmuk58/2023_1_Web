9c0ddd9bf361dca28c68338c6a375f874840de3a
Nakuri <ccmuk58@naver.com>
2023-06-17 04:13:00 +0900

    Fix: addcart Snack and Beverage

--- innerPage/addcart.php
+++ innerPage/addcart.php
@@ -18,17 +18,17 @@ $menutype = $_GET['menutype'];
 		<br><br><br>
 		<div class="container">
 			<div class="row">
+				
 				<?php
 				if ($menutype == "burger") {
 					$name = $_GET['burger'];
 					$single = $_GET['singlemenu'];
 					$set = $_GET['setmenu']; ?>
+					
 					<div class="col-75">
 						<?= $name ?>
 					</div>
 					<div class="col-25">
-						<input type="text" name="menutype" value="<?= $menutype ?>" hidden>
-						<input type="text" name="name" value="<?= $name ?>" hidden>
 						<input type="radio" name="size" value="single" checked>싱글(<?= $single ?>)
 						<input type="radio" name="size" value="set">세트(<?= $set ?>)
 						<input type="text" name="singleprice" value="<?= $single ?>" hidden>
@@ -47,10 +47,8 @@ $menutype = $_GET['menutype'];
 						<?= $name ?>
 					</div>
 					<div class="col-25">
-						<input type="radio" name="size" value="small" checked>S(
-						<?= $small ?>)
-						<input type="radio" name="size" value="large">L(
-						<?= $large ?>)
+						<input type="radio" name="size" value="small" checked>
+						S(<?= $small ?>)<input type="radio" name="size" value="large">L(<?= $large ?>)
 						<input type="text" name="small" value="<?= $small ?>" hidden>
 						<input type="text" name="large" value="<?= $large ?>" hidden>
 					</div>
@@ -65,8 +63,7 @@ $menutype = $_GET['menutype'];
 						<?= $name ?>
 					</div>
 					<div class="col-25">
-						<input type="radio" name="price" value="price" checked>가격(
-						<?= $price ?>)
+						<input type="radio" name="price" value="<?= $price ?>" checked>가격(<?= $price ?>)
 					</div>
 					<div class="col-25">
 						<input type="number" name="qty" value="1" min="1" max="10">
@@ -74,6 +71,9 @@ $menutype = $_GET['menutype'];
 					<?php
 				} ?>
 			</div>
+			
+			<input type="text" name="menutype" value="<?= $menutype ?>" hidden>
+			<input type="text" name="name" value="<?= $name ?>" hidden>
 			<div class="row">
 				<input type="submit" value="Put In" class="btn">
 			</div>
