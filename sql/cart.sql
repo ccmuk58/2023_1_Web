9421e57f805c8f773a3f98fbf8f5d3cb7abdc056
RIHU <rihu0311@gmail.com>
2023-06-17 02:40:36 +0900

    ㅇ

--- sql/cart.sql
+++ sql/cart.sql
@@ -1,8 +1,8 @@
-Create DATABASE IF NOT EXISTS 'burger';
+CREATE DATABASE IF NOT EXISTS `burger`;
 
-USE 'burger';
+USE `burger`;
 
-DROP TABLE IF EXISTS 'cart';
+DROP TABLE IF EXISTS `cart`;
 
 CREATE TABLE `cart` (
   `email` varchar(30) NOT NULL,
@@ -10,5 +10,6 @@ CREATE TABLE `cart` (
   `size` char(6) DEFAULT NULL,
   `qty` int(2) NOT NULL,
   `price` int(11) NOT NULL,
-  PRIMARY KEY ('email','food', 'size')
+  `total` int(11) NOT NULL,
+  PRIMARY KEY (`email`,`food`, `size`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
