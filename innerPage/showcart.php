8f4a1b1d472b9c30035a7138d87a360bdbc93772
Dolms <kimms92405320@gmail.com>
2023-06-17 04:08:23 +0900

    showcart

--- innerPage/showcart.php
+++ innerPage/showcart.php
@@ -1,57 +1,44 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            body {
-                    text-align: left;
-                }
-            #burger {
-                font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-                border-collapse: collapse;
-                width: 70%;
-                margin-left: auto;
-                margin-right: auto;
-                }
-            #burger td, #burger th {
-                border: 1px solid #ddd;
-                padding: 8px;
-            }
-            #burger tr:hover {background-color: #ddd;}
-            .btn {
-                background-color: #E8BB44;
-                color: white;
-                padding: 16px 20px;
-                border: none;
-                cursor: pointer;
-                width: 70%;
-                opacity: 0.9;
-                margin-top: 10px;
-            }
-            .btn:hover {
-                opacity: 1;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Shoppingcart List</h1>
-        <?php
-        session_start();
-        $email = $_SESSION['pz_uid'];
-        include_once('dbconn.php');
-        $sql = "select * from cart where email = '$email'";
-        $result = $conn->query($sql);
-        if(!$result)
-            die("cart 테이블 검색 오류");
-        ?>
-        <form action="removecart.php" method="post">
-        <table id="burger">
+<link rel="stylesheet" href="../css/style.css"><!-- 네비게이션바 style.php css적용-->
+<link rel="stylesheet" href="../css/showcartStyle.css"><!-- addcart.php css적용-->
+
+<?php
+session_start();
+$email = $_SESSION['pz_uid'];
+include_once('dbconn.php');
+$sql = "select * from cart where email = '$email'";
+$result = $conn->query($sql);
+if(!$result)
+    die("cart 테이블 검색 오류");
+?>
+
+<div id="navbar">
+	<a href="" id="logo">Z</a>
+</div>
+<div id="container">
+    <form action="removecart.php" method="post">
+        <table id="cart">
             <tr>
-                <th></th><th>NO</th><th>Burger</th><th>Size</th><th>Quantity</th><th>Price</th>
+                <th></th><th>NO</th><th>Menu</th><th>Size</th><th>Quantity</th><th>Price</th>
             </tr>
-
-
-
-</html>
+        <?php
+            $no = 0;
+            while($row = $result->fetch_assoc()){
+                $no++;
+                ?>
+                <tr>
+                    <td>
+                        <input type="checkbox" name="chk[]"
+                        value="<?=$row['food']?>@<?=$row['size']?>">
+                    </td>
+                    <td><?=$no?></td>
+                    <td><?=$row['food']?></td>
+                    <td><?=$row['size']?></td>
+                    <td><?=$row['qty']?></td>
+                    <td><?=$row['total']?></td>
+                </tr>
+        <?php } ?>
+        </table>
+        <input type="submit" value="Delete Cart" class="btn">
+    </form>
+    <button class="btn" onclick="location.href='ordernew.php'">Order</button>
+</div>
