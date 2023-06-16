850bce319a2bfcf2503675f278fd7141c51e9bfa
RIHU <rihu0311@gmail.com>
2023-06-17 02:29:31 +0900

    cart테이블

---
+++ sql/cart.sql
@@ -0,0 +1,14 @@
+Create DATABASE IF NOT EXISTS 'burger';
+
+USE 'burger';
+
+DROP TABLE IF EXISTS 'cart';
+
+CREATE TABLE `cart` (
+  `email` varchar(30) NOT NULL,
+  `food` varchar(20) NOT NULL,
+  `size` char(6) DEFAULT NULL,
+  `qty` int(2) NOT NULL,
+  `price` int(11) NOT NULL,
+  PRIMARY KEY ('email','food', 'size')
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
